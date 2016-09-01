6e2d290276c5928fedcd78b90b1195c823f318ff
李林 <ww1095@163.com>
2016-09-01 17:34:18 +0800

    1

---
+++ src/com/anli/userinfoui/UserInfoUIActivity.java
@@ -0,0 +1,13 @@
+package com.anli.userinfoui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserInfoUIActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
